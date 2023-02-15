8e928ca28cfe308e3064cbff822a2cad4c654a09
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-02-15 11:28:16 +0530

    commit

---
+++ GFG_Course_DSA/src/arrays2D_Questions/SumOfDiagonalMatrix.java
@@ -0,0 +1,41 @@
+package arrays2D_Questions;
+
+import java.util.Scanner;
+
+public class DiagonalMatrix {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int arr[][] = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		diagonalMatrix(arr, n);
+	}
+
+	private static void diagonalMatrix(int[][] arr, int n) {
+
+		int sum = 0, Secsum = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == j) {
+					sum += arr[i][j];
+				}
+				if (i + j == n-1) {
+					Secsum += arr[i][j];
+				}
+			}
+		}
+		System.out.println(sum + " " + Secsum);
+
+	}
+
+}
