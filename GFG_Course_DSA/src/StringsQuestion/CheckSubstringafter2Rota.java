c6ac667d4b057809996646a2a7efc893b3b5c8cd
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-02-17 00:26:42 +0530

    Update CheckSubstringafter2Rota.java

--- GFG_Course_DSA/src/StringsQuestion/CheckSubstringafter2Rota.java
+++ GFG_Course_DSA/src/StringsQuestion/CheckSubstringafter2Rota.java
@@ -6,19 +6,42 @@ public class CheckSubstringafter2Rota {
 
 	public static void main(String[] args) {
 
-		//Scanner sc = new Scanner(System.in);
-		 String str="rellnmpapqfwkhop";
-	        String str2="llnmpapqfwkhopre";
-	        char c=str.charAt(0);
-	        char d=str.charAt(1);
+		Scanner sc = new Scanner(System.in);
 
-	        String bag="";
-	        for(int i=2;i<str.length();i++){
-	            bag+=str.charAt(i);
-	        }
-	        bag=bag+c+d;
-//	        System.out.print(bag+c+d);
-	        System.out.println(bag.equals(str2));
+		String str1 = sc.next();
+		String str2 = sc.next();
+
+		if (checkSubstringAfterRotation(str1, str2)) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+	}
+
+	private static boolean checkSubstringAfterRotation(String str1, String str2) {
+
+		if (str1.length() != str2.length()) {
+			return false;
+		}
+		if (str1.length() <= 2 || str2.length() <= 2) {
+			return (str1.equals(str2));
+		}
+
+		String clock_rot = "";
+		String antiClock_rot = "";
+		int len = str1.length();
+
+		clock_rot = str1.substring(len - 2) + str1.substring(0, len - 2);
+		System.out.println(clock_rot);
+
+		antiClock_rot = str1.substring(2, len) + str1.substring(0, 2);
+		System.out.println(antiClock_rot);
+
+		if (str2.equals(antiClock_rot) || str2.equals(clock_rot)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 
