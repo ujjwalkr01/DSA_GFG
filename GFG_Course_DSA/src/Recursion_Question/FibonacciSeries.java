96bdaaeea063148ea6c6836ee59a59276f625625
ujjwl_kr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-02-18 18:00:14 +0530

    Add files via upload

---
+++ GFG_Course_DSA/src/Recursion_Question/FibonacciSeries.java
@@ -0,0 +1,23 @@
+package Recursion_Question;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		System.out.println(fib(n));
+	}
+
+	private static int fib(int n) {
+
+		if (n == 0 || n == 1) {
+			return n;
+		}
+		return fib(n - 1) + fib(n - 2);
+	}
+
+}
