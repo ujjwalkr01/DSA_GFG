411074c36457f330013f5c6b7eb2180b4a6488dd
ujjwl_kr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-03-01 10:39:01 +0530

    Update Introduction.java

--- GFG_Course_DSA/src/CircularLinkedList/Introduction.java
+++ GFG_Course_DSA/src/CircularLinkedList/Introduction.java
@@ -1,10 +1,36 @@
 package CircularLinkedList;
 
+class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		next = null;
+	}
+}
+
 public class Introduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Node head = new Node(10);
+		head.next = new Node(30);
+		head.next.next = new Node(20);
+		head.next.next.next = new Node(40);
+		head.next.next.next.next = head;
+
+		printList(head);
+
+	}
+
+	private static void printList(Node head) {
+
+		Node curr = head;
+		while (curr != null) {
+			System.out.print(curr.data + " ");
+			curr = curr.next;
+		}
 	}
 
 }
