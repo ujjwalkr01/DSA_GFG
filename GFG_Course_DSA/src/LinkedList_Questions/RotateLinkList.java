c717899716a1668087a88c1290734114dbf0d3a6
ujjwl_kr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-03-09 01:27:54 +0530

    Add files via upload
    commit

---
+++ GFG_Course_DSA/src/LinkedList_Questions/RotateLinkList.java
@@ -0,0 +1,81 @@
+package LinkedList_Questions;
+
+class Node8 {
+	int data;
+	Node8 next;
+
+	public Node8(int data) {
+		this.data = data;
+		next = null;
+	}
+}
+
+public class RotateLinkList {
+
+	public static void main(String[] args) {
+
+		Node8 head = new Node8(2);
+		head.next = new Node8(4);
+		head.next.next = new Node8(7);
+		head.next.next.next = new Node8(8);
+		head.next.next.next.next = new Node8(9);
+//		head.next.next.next.next.next = new Node8(6);
+//		head.next.next.next.next.next.next = new Node8(7);
+//		head.next.next.next.next.next.next.next = new Node8(8);
+
+		printList(head);
+		System.out.println("After rotating the element Kth times");
+		int k = 3;
+		head = rotateTheList(head, k);
+		printList(head);
+	}
+
+	private static Node8 rotateTheList(Node8 head, int k) {
+
+		Node8 dummy = new Node8(0);
+
+		int count = getCount(head);
+		if (k >= count) {
+			return head;
+		}
+
+		Node8 curr = head, ptr = dummy;
+		for (int i = 1; i <= k; i++) {
+			ptr.next = curr;
+			ptr = ptr.next;
+			curr = curr.next;
+			ptr.next = null;
+		}
+		head = curr;
+		Node8 temp = head;
+
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = dummy.next;
+
+		return head;
+	}
+
+	private static int getCount(Node8 head) {
+
+		Node8 curr = head;
+		int count = 0;
+
+		while (curr != null) {
+			count++;
+			curr = curr.next;
+		}
+		return count;
+	}
+
+	private static void printList(Node8 head) {
+
+		Node8 ptr1 = head;
+		while (ptr1 != null) {
+			System.out.print(ptr1.data + " ");
+			ptr1 = ptr1.next;
+		}
+	}
+
+}
